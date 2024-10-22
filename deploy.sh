d448f308c185f0968a8fa6b91aaae0f51b2e2e97
Kaleeswaran V <vkalees64@gmail.com>
2024-10-22 15:17:02 +0530

    fix: deploy.sh file update

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,19 @@
+#cd ../
+#rm -rf todoapp-angular
+#git clone https://github.com/kalees64/todoapp-angular.git
+#cd todoapp-angular/
 git pull
 echo -e "\ngit pull successfull!!! \n"
 rm -rf node_modules
-echo -e "node_modules  deleted!!! \n"
+echo -e "node_modules deleted!!! \n"
 npm install --force
 echo -e  "\nnpm install --force successful!!! \n"
 rm -rf dist
 echo -e "existing build files are removed from /todoapp-angular/dist/todoapp/browser/ successfully!!! \n"
 ng build
 echo -e "Build successful!!! \n"
-cd /var/www/projects/root/
-rm -rf *
+rm -rf /var/www/projects/root/*
 echo -e "all existing files are removed from the /var/www/projects/root/ folder successfully!!! \n"
 cp -r /todoapp-angular/dist/todoapp/browser/** /var/www/projects/root/
 echo -e "dist folder files copied and pasted /var/www/projects/root/ successfully!!! \n"
+echo  -e "Build and deploy process completed successfully!!! \n"
